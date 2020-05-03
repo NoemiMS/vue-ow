8153b2cbc66f205d8b5e4ad48d26003cd60ea178
unknown <noemims@gmail.com>
2020-05-03 11:27:23 +0200

    starting project

---
+++ src/components/CoDeveloper.vue
@@ -0,0 +1,12 @@
+<template>
+    
+</template>
+<script>
+export default {
+    name: 'CoDeveloper',
+    props: ['avatar',]
+}
+</script>
+<style lang="scss">
+
+</style>
---
+++ src/components/CoDevelopers.vue
@@ -0,0 +1,21 @@
+<template>
+    <ul class="developers">
+        <li class="developers__item">
+            <co-developer></co-developer>
+        </li>
+    </ul>
+</template>
+<script>
+export default {
+     name: 'CoDevelopers',
+     data() {
+         return {
+             user: [], //nuestro componente va a tener un valor inicial users, que es un array que comienza vacío
+
+         } 
+     }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
